067cf8ab90f5dc435d428a4764a9937062871747
felixggj <me@felix-gomez.com>
2023-10-17 12:56:09 +0200

    Everything works correctly

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/felix/Documents/Master folder/IE university/All Courses/3rd year/1st semester/Programming II/assignments/assignment1
-# Build directory: /Users/felix/Documents/Master folder/IE university/All Courses/3rd year/1st semester/Programming II/assignments/assignment1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
